c73ef20d8a62895d1b9ac6d41c962a6e6a1dd007
Naveen Setty <naveenkumarpg@gmail.com>
2021-03-29 16:38:04 +0100

    path example

---
+++ PathExample/bin/PathExample.dart
@@ -0,0 +1,7 @@
+import'package:path/path.dart';
+
+void main(){
+  var path=join('Directory','filename.txt');
+  print(path);
+}
+
---
+++ PathExample/test/PathExample_test.dart
@@ -0,0 +1,8 @@
+import 'package:PathExample/PathExample.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
